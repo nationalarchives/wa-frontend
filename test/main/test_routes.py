c0d6049b6513f2f54db04b02390abd65d3536368
Ian Meigh <ian.meigh@torchbox.com>
2026-01-08 19:27:19 +0000

    fix: remove index page test

--- test/main/test_routes.py
+++ test/main/test_routes.py
@@ -18,11 +18,6 @@ class MainBlueprintTestCase(unittest.TestCase):
         self.assertEqual(rv.status_code, 308)
         self.assertEqual(rv.location, f"{self.domain}/healthcheck/live/")
 
-    def test_homepage(self):
-        rv = self.app.get("/")
-        self.assertEqual(rv.status_code, 200)
-        self.assertIn('<h1 class="tna-heading-xl">TNA Flask application</h1>', rv.text)
-
     def test_cookies(self):
         rv = self.app.get("/cookies/")
         self.assertEqual(rv.status_code, 200)
