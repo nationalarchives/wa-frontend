65e268e9cd6193b96460dd65c45f118b090ac063
Ian Meigh <ian.meigh@torchbox.com>
2026-01-23 12:55:14 +0000

    feat: add a-to-z routes and basic templates

--- app/wagtail/render.py
+++ app/wagtail/render.py
@@ -2,17 +2,20 @@ from flask import current_app, render_template
 from pydash import objects
 
 from .pages import (
+    atoz_archive_page,
     home_page,
     information_page,
     listing_page,
     section_index_page,
 )
 
+
 page_type_templates = {
     "ukgwa.UKGWAHomePage": home_page.render_home_page,
     "ukgwa.SectionIndexPage": section_index_page.render_section_index_page,
     "ukgwa.InformationPage": information_page.render_information_page,
     "ukgwa.ListingPage": listing_page.render_listing_page,
+    "ukgwa.AToZArchivePage": atoz_archive_page.render_atoz_archive_page,
 }
 
 
